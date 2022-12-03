61f5487b7e1f3b06d0377c29bcea3622a2b24638
David Price <Dpricedev@gmail.com>
2022-12-03 19:27:29 +0000

    Neated day 03

--- src/day03/Day03.cpp
+++ src/day03/Day03.cpp
@@ -5,95 +5,43 @@
 #include <numeric>
 #include "file_utils.h"
 
-auto findMatchingTypesIn(const std::string_view& line, auto&& onPriority) {
-    for (const auto & letter : line) {
-        const auto value = static_cast<int>(letter ^ ' ');
-        const auto priority = value - 64 - ((value / 91) * 6);
-        onPriority(priority);
-    }
+constexpr auto getPriority(const char& letter) -> int {
+    const auto asciiValue = static_cast<int>(letter ^ ' ');
+    return asciiValue - 64 - ((asciiValue / 91) * 6);
 }
 
-auto findMatchingTypesIn(std::vector<std::string_view>&& lines) -> int {
-    std::array<int, 52> array { };
-    std::for_each(lines.begin(), lines.end() - 1, [&array, index = 1] (const auto & line) mutable {
+constexpr auto findMatchingTypesIn(std::vector<std::string_view>&& lines) -> int {
+    std::array<int, 52> types { };
 
-        findMatchingTypesIn(line, [&array, &index] (const auto & priority) {
-            // todo: Open add one if index is 1 less than current priority
-            if (array[priority - 1] == index - 1) {
-                array[priority - 1] += 1;
-            }
-        });
+    for (std::size_t index = 0; index < lines.size() - 1; ++index) {
+        for (const auto& letter : lines[index]) {
+            const auto priority = getPriority(letter);
+            if (types[priority - 1] == index) types[priority - 1] += 1;
+        }
+    }
 
-//        for (const auto & letter : line) {
-//            const auto value = static_cast<int>(letter ^ ' ');
-//            const auto priority = value - 64 - ((value / 91) * 6);
-//
-//            // todo: only increment if previous had value
-//            if (array[priority - 1] == index - 1) {
-//                array[priority - 1] += 1;
-//            }
-//        }
-        ++index;
+    const auto line = lines.back();
+    return std::accumulate(line.begin(), line.end(), 0, [&types, &lines] (const auto& sum, const auto& letter) {
+        const auto priority = getPriority(letter);
+        const auto prioritySum = types[priority - 1] / (lines.size() - 1) * priority;
+        types[priority - 1] = 0;
+        return prioritySum + sum;
     });
-
-    auto sum = 0;
-    findMatchingTypesIn(lines.back(), [&array, &sum, &lines] (const auto & priority) mutable {
-        sum += (array[priority - 1] / (lines.size() - 1)) * priority;
-        array[priority - 1] = 0;
-    });
-//
-//    for(const auto & letter : lines.back()) {
-//        const auto value = static_cast<int>(letter ^ ' ');
-//        const auto priority = value - 64 - ((value / 91) * 6);
-//        sum += (array[priority - 1] / (lines.size() - 1)) * priority;
-//        array[priority - 1] = 0;
-//    }
-
-    return sum;
 }
 
 auto main() -> int {
     const auto lines = file::getLines("input.txt");
 
     const auto partOne = std::accumulate(lines.begin(), lines.end(), 0, [] (const auto& sum, const auto& line) {
-        return sum + findMatchingTypesIn(std::vector<std::string_view> {
-            { line.begin(), line.begin() + line.length() / 2 },
-            { line.begin() + line.length() / 2, line.end() }
-        });
+        const auto middleIterator = line.begin() + line.length() / 2;
+        return sum + findMatchingTypesIn({ { line.begin(), middleIterator }, { middleIterator, line.end() } });
     });
 
     auto partTwo = 0;
-    for (int i = 0; i < lines.size(); i = i + 3) {
-        partTwo += findMatchingTypesIn(std::vector<std::string_view> { lines[i], lines[i + 1], lines[i + 2] });
+    for (std::size_t index = 0; index < lines.size(); index += 3) {
+        partTwo += findMatchingTypesIn({ lines[index], lines[index + 1], lines[index + 2] });
     }
 
     std::cout << "Part 1: " << partOne << '\n' << "Part 2: " << partTwo;
-
-//    auto sum = 0;
-//    for(const auto & line : lines) {
-//        auto array = std::array<int, 52> { };
-//
-//        // todo: Could alter to be a function that takes a vector or array of input strings?
-//        // todo: or maybe a set of string views?
-//        // todo: and then the array stores the number up to size - 1 and checks if it == size - 1
-//        // in that case need to do first loop for all items and last loop for last item
-//
-//        // todo: Could do some sort of running index or over half check here
-//        const auto lineHalf = line.begin() + (line.length() / 2);
-//        std::for_each(line.begin(), lineHalf, [&array] (const auto & letter) {
-//            const auto value = static_cast<int>(letter ^ ' ');
-//            const auto priority = value - 64 - ((value / 91) * 6);
-//            array[priority - 1] = 1;
-//        });
-//
-//        std::for_each(lineHalf, line.end(), [&array, &sum] (const auto & letter) {
-//            const auto value = static_cast<int>(letter ^ ' ');
-//            const auto priority = value - 64 - ((value / 91) * 6);
-//            sum += array[priority - 1] * priority;
-//            array[priority - 1] = 0;
-//        });
-//    }
-//
-//    std::cout << "Part 1: " << sum << '\n' << "Part 2: " << 2;
     return 0;
 }
