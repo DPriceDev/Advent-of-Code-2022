65d6cc7f16148aba482c6f198068410c31c6f7ed
David Price <Dpricedev@gmail.com>
2022-12-04 18:55:28 +0000

    Day 04

--- src/day03/Day03.cpp
+++ src/day03/Day03.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <set>
 #include <functional>
 #include <array>
 #include <numeric>
 #include "file_utils.h"
 
-constexpr auto getPriority(const char& letter) -> int {
+auto getPriority(const char& letter) -> int {
     const auto asciiValue = static_cast<int>(letter ^ ' ');
     return asciiValue - 64 - ((asciiValue / 91) * 6);
 }
 
-constexpr auto findMatchingTypesIn(std::vector<std::string_view>&& lines) -> int {
+constexpr auto findMatchingTypes(std::vector<std::string_view>&& lines) -> int {
     std::array<int, 52> types { };
 
     for (std::size_t index = 0; index < lines.size() - 1; ++index) {
@@ -20,26 +21,27 @@ constexpr auto findMatchingTypesIn(std::vector<std::string_view>&& lines) -> int
         }
     }
 
-    const auto line = lines.back();
-    return std::accumulate(line.begin(), line.end(), 0, [&types, &lines] (const auto& sum, const auto& letter) {
+    auto sum = 0;
+    for(const auto& letter: lines.back()) {
         const auto priority = getPriority(letter);
-        const auto prioritySum = types[priority - 1] / (lines.size() - 1) * priority;
+        sum += types[priority - 1] / (lines.size() - 1) * priority;
         types[priority - 1] = 0;
-        return prioritySum + sum;
-    });
+    }
+    return sum;
 }
 
 auto main() -> int {
     const auto lines = file::getLines("input.txt");
 
-    const auto partOne = std::accumulate(lines.begin(), lines.end(), 0, [] (const auto& sum, const auto& line) {
+    auto partOne = 0;
+    for (const auto  & line : lines) {
         const auto middleIterator = line.begin() + line.length() / 2;
-        return sum + findMatchingTypesIn({ { line.begin(), middleIterator }, { middleIterator, line.end() } });
-    });
+        partOne += findMatchingTypes({ {line.begin(),   middleIterator}, {middleIterator, line.end()} });
+    }
 
     auto partTwo = 0;
     for (std::size_t index = 0; index < lines.size(); index += 3) {
-        partTwo += findMatchingTypesIn({ lines[index], lines[index + 1], lines[index + 2] });
+        partTwo += findMatchingTypes({lines[index], lines[index + 1], lines[index + 2]});
     }
 
     std::cout << "Part 1: " << partOne << '\n' << "Part 2: " << partTwo;
---
+++ src/day04/Day04.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <array>
+#include "file_utils.h"
+#include <tuple>
+#include <regex>
+
+const std::regex regex { R"((\d+)-(\d+),(\d+)-(\d+))" };
+
+auto parsePairs(const std::string& line) -> std::tuple<int, int, int, int> {
+    std::smatch match;
+    std::regex_match(line.begin(), line.end(), match, regex);
+    return std::make_tuple(stoi(match[1]), stoi(match[2]), stoi(match[3]), stoi(match[4]));
+}
+
+auto main() -> int {
+    const auto lines = file::getLines("input.txt");
+
+    auto partOne = 0;
+    for (const auto & line : lines) {
+        auto [a1, a2, b1, b2] = parsePairs(line);
+        if ((a1 >= b1 && a1 <= b2 && a2 >= b1 && a2 <= b2) || (b1 >= a1 && b1 <= a2 && b2 >= a1 && b2 <= a2)) ++partOne;
+    }
+
+    auto partTwo = 0;
+    for (const auto & line : lines) {
+        auto [a1, a2, b1, b2] = parsePairs(line);
+        if ((a1 >= b1 && a1 <= b2 || a2 >= b1 && a2 <= b2) || (b1 >= a1 && b1 <= a2 || b2 >= a1 && b2 <= a2)) ++partTwo;
+    }
+
+    std::cout << "Part 1: " << partOne << '\n' << "Part 2: " << partTwo;
+    return 0;
+}
