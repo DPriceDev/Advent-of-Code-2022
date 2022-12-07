cfcc8ea454d352ac75b8e21a9586bde9f7cca7aa
David Price <Dpricedev@gmail.com>
2022-12-07 21:12:16 +0000

    Refactored day 7 to use ranges and indexes instead of pointers

--- src/day07/Day07.cpp
+++ src/day07/Day07.cpp
@@ -1,135 +1,100 @@
-#include <iostream>
 #include "file_utils.h"
-#include <utility>
+#include <iostream>
 #include <vector>
 #include <ranges>
 #include <algorithm>
 #include <numeric>
-#include <sstream>
 #include <map>
-
-struct File {
-    std::string name;
-    std::size_t size;
-
-    File(std::string name, std::size_t size) : name(name), size(size) { }
-};
-
-// todo: if adding a file, call add size of parent and current directory?
+#include <sstream>
 
 struct Directory {
-    Directory* parent;
-    std::vector<File> files;
-    std::map<std::string, Directory> directories;
-
-    [[nodiscard]]
-    std::size_t getSize() const {
-        const auto getFileSize = [] (const auto total, const auto& next) { return total + next.size; };
-        const auto fileSize = std::accumulate(files.begin(), files.end(), 0, getFileSize);
-
-        const auto getDirSize = [] (const auto total, auto& next) { return total + next.second.getSize(); };
-        const auto dirSize = std::accumulate(directories.begin(), directories.end(), 0, getDirSize);
-
-        return fileSize + dirSize;
-    }
-
-    [[nodiscard]]
-    std::vector<std::size_t> getAllSizes() const {
-        std::vector<std::size_t> sizes;
-
-        sizes.push_back(getSize());
-
-        for (const auto& directory : directories) {
-            for (const auto & size : directory.second.getAllSizes()) {
-                sizes.push_back(size);
-            }
-        }
-
-        return sizes;
-    }
-
-    auto getDirectory(std::string name) -> Directory& {
-        auto directory = std::find_if(directories.begin(), directories.end(), [&] (auto& directory) {
-            return directory.first == name;
-        });
-
-        if (directory == directories.end()) {
-            directories[name] = Directory { .parent = this };
-            return directories[name];
-        }
-
-        return directory->second;
-    }
-
-    auto addDirectory(std::string name) {
-        getDirectory(name);
-    }
-
-    auto addFile(std::string& name, std::size_t size) {
-        files.emplace_back(name, size);
-    }
+    bool isRoot = false;
+    std::size_t parentId;
+    std::map<std::string, std::size_t> directories { };
+    std::size_t size = 0;
 };
 
+class FileSystem {
+    std::vector<Directory> directories { Directory { .isRoot = true } };
+    std::size_t root = 0;
+    std::size_t current = root;
 
-auto findMaxSizeOf(const Directory& directory) -> std::size_t {
-    auto sizes = directory.getAllSizes();
-
-    auto lowest = [] (const auto& size) { return size <= 100000; };
-//    std::ranges::sort(sizes);
-    auto count = 0;
-    for(const auto& size : sizes) {
-        if (size <= 100000) {
-            count += size;
-        }
+    auto addFile(std::size_t index, std::size_t size) -> void {
+        directories[index].size += size;
+        if (!directories[index].isRoot) addFile(directories[index].parentId, size);
     }
 
-    return count;
-}
+public:
+    auto addDirectory(const std::string& name) {
+        directories.push_back({ .parentId = current });
+        getCurrentDirectory()->directories[name] = directories.size() - 1;
+    }
 
-auto parseCommands(std::vector<std::string>& commands) -> Directory {
-    Directory base { };
-    Directory* current = &base;
+    auto addFile(const std::size_t size) { addFile(current, size); }
 
+    auto getDirectories() -> std::vector<Directory>& { return directories; }
+
+    auto getRootDirectory() -> Directory* { return &directories[root]; }
+
+    auto getCurrentDirectory() -> Directory* { return &directories[current]; }
+
+    auto setDirectory(const std::string&& name) { current = getCurrentDirectory()->directories[name]; }
+
+    auto goUp() { current = getCurrentDirectory()->parentId; }
+
+    auto goToRoot() { current = root; }
+};
+
+auto generateFileSystem(std::vector<std::string>& commands) -> FileSystem {
+    FileSystem fileSystem;
     for(const auto& command : commands) {
-
-        if (command == ("$ cd /")) { current = &base; }
-        else if (command == ("$ cd ..")) { current = current->parent; }
+        if (command == ("$ cd /")) fileSystem.goToRoot();
+        else if (command == ("$ cd ..")) fileSystem.goUp();
+        else if (command.starts_with("$ ls")) continue;
         else if (command.starts_with("$ cd")) {
-            current = &current->getDirectory(std::string { command.begin() + 4, command.end() });
+            fileSystem.setDirectory({ command.begin() + 4, command.end() });
         }
-        else if (command.starts_with("$ ls")) { /* no - op*/ }
         else if (command.starts_with("dir")) {
-            current->addDirectory(std::string { command.begin() + 3, command.end() });
-        }
-        else {
+            fileSystem.addDirectory({ command.begin() + 3, command.end() });
+        } else {
             std::stringstream sstream { command };
             std::size_t size = 0;
-            std::string name;
             sstream >> size;
-            sstream >> name;
-            current->addFile(name, size);
+            fileSystem.addFile(size);
         }
     }
+    return fileSystem;
+}
 
-    return base;
+constexpr auto toSize = [] (const auto& directory) { return directory.size; };
+
+auto findMaxSizeOf(FileSystem& fileSystem) -> std::size_t {
+    constexpr auto lessThan = [] (const auto size) { return size <= 100000; };
+    auto filtered = fileSystem.getDirectories()
+                    | std::views::transform(toSize)
+                    | std::views::filter(lessThan);
+
+    return std::reduce(filtered.begin(), filtered.end());
+}
+
+auto findDirectoryToDelete(FileSystem& fileSystem) -> std::size_t {
+    const auto baseSize = fileSystem.getRootDirectory()->size;
+    const auto freeSpace = [&] (const auto size) { return ((70000000 - baseSize) + size) >= 30000000; };
+
+    auto sizes = fileSystem.getDirectories()
+                 | std::views::transform(toSize)
+                 | std::views::filter(freeSpace);
+
+    return std::ranges::min(sizes);
 }
 
 auto main() -> int {
     auto commands = file::getLines("input.txt");
+    auto fileSystem = generateFileSystem(commands);
 
-    const auto baseDirectory = parseCommands(commands);
+    const auto partOne = findMaxSizeOf(fileSystem);
+    const auto partTwo = findDirectoryToDelete(fileSystem);
 
-    const auto partOne = findMaxSizeOf(baseDirectory);
-
-    auto sizes = baseDirectory.getAllSizes();
-    const auto baseSize = baseDirectory.getSize();
-    std::ranges::sort(sizes);
-    auto test = std::find_if(sizes.begin(), sizes.end(), [&baseSize] (const auto& size) {
-        auto test = ((70000000 - baseSize) + size);
-        return test >= 30000000;
-    });
-
-    std::cout << "part 1: " << partOne << "\npart 2: " << *test;
-
+    std::cout << "part 1: " << partOne << "\npart 2: " << partTwo;
     return 0;
 }
