543f33f9d8796b26862171307bc164500f3032f3
David Price <Dpricedev@gmail.com>
2022-12-10 09:44:44 +0000

    day 10

---
+++ src/day10/Day10.cpp
@@ -0,0 +1,44 @@
+#include "file_utils.h"
+#include <iostream>
+#include <vector>
+#include <charconv>
+
+auto parse(const std::vector<std::string>& lines, const auto&& onAction) {
+    auto cycle = 0;
+    auto x = 1;
+    for (const auto& line : lines) {
+        if (line == "noop") onAction(cycle, x);
+        else if (line.starts_with("addx")) {
+            int value;
+            std::from_chars(line.data() + 5, line.data() + line.size(), value);
+            for (int i = 0; i < 2; ++i) onAction(cycle, x);
+            x += value;
+        }
+    }
+}
+
+auto partOne(const std::vector<std::string>& lines) -> int {
+    auto multiple = 0;
+    parse(lines, [&] (auto& cycle, const auto& x) {
+        ++cycle;
+        if ((cycle - 20) % 40 == 0) multiple += (x * cycle);
+    });
+    return multiple;
+}
+
+auto partTwo(const std::vector<std::string>& lines) {
+    parse(lines, [&] (auto& cycle, const auto& x) {
+        if (cycle % 40 == 0) std::cout << "\n";
+        const auto current = cycle - (40 * (cycle / 40));
+        std::cout << ((x >= current - 1 && x <= current + 1) ? '#' : ' ');
+        ++cycle;
+    });
+}
+
+auto main() -> int {
+    auto lines = file::getLines("input.txt");
+    const auto one = partOne(lines);
+    std::cout << "\npart 1: " << one << "\npart 2:";
+    partTwo(lines);
+    return 0;
+}
